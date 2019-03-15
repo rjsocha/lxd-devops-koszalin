9eb0ee8570af789f3711aa1732130fa365c1245d
Robert Socha <socha@socha.it>
2019-03-15 21:23:47 +0100

    apka xhr

---
+++ html/xhr-my-vm.php
@@ -0,0 +1,68 @@
+<?php
+	require('f.php');
+	session_start();
+	if(!isset($_SESSION['namespace'])) {
+		header('HTTP/1.0 403 Forbidden');
+		die();
+	}
+	$ns=$_SESSION['namespace'];
+	$ns_dir = "/home/socha/.vm/namespace/" . $ns;
+
+	$p=get_pool($ns);
+	sort($p);
+	$ret = array();
+	$idx=0;
+	foreach($p as $vm) {
+		$vi = get_vm_info($vm);
+		if(count($vi)<1) continue;
+		$vi = $vi[0];
+		$status = strtolower($vi['status']);
+		$ret[$idx]['have_ip6']=0;
+		$ret[$idx]['name']=$vm;
+		$ret[$idx]['state']=$status;
+		$ret[$idx]['domain']='lxd.nauka.ga';
+		$ret[$idx]['architecture']=$vi['architecture'];
+		$ret[$idx]['created_at']=strtotime($vi['created_at']);
+		$ret[$idx]['id']=md5($ret[$idx]['created_at'] . $ret[$idx]['name']);
+		if($status=="running") {
+			$has_ip6_global=0;
+			$ret[$idx]['data']['memory']=$vi['state']['memory'];
+			foreach($vi['state']['network'] as $net => $key) {
+				if($net == "lo" || $net == "sit0")
+					continue;
+				if(isset($key['host_name'])) unset($key['host_name']);
+				$ret[$idx]['data']['network'][$net]=$key;
+			}
+			if(count($ret[$idx]['data']['network'])) {
+				foreach($ret[$idx]['data']['network'] as $net_if) {
+					if(isset($net_if['addresses'])) {
+						foreach($net_if['addresses'] as $addr) {
+							if($addr['scope']=="global") {
+								if($addr['family']=="inet6") $has_ip6_global=1;
+								$ret[$idx]['network'][$addr['family']][]=array("address"=>$addr['address'],"netmask"=>$addr['netmask']);
+							}
+					       }
+					}
+				}
+			}
+			$handle = fopen($ns_dir . "/vm/" . $vm, "r");
+			$vm_pass="-";
+			if ($handle) {
+				while (($line = fgets($handle)) !== false) {
+					if(preg_match("/^LXD_PASS=(.+)$/",$line,$match)) {
+						$vm_pass = $match[1];
+						}
+				}
+				fclose($handle);
+			}
+			$ret[$idx]['data']['os']['distribution']='Ubuntu 18.04 LTS';
+			$ret[$idx]['data']['os']['username']='ubuntu';
+			$ret[$idx]['data']['os']['password']=$vm_pass;
+			if($has_ip6_global) { 
+				$ret[$idx]['have_ip6']=1;
+			}
+
+		}
+		$idx++;
+	}
+	echo json_encode($ret);
